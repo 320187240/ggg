61e043ebb5cef29146d638d720caa452e6619799
320187 <320187240@qq.com>
2024-09-22 10:34:49 +0800

    change

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import {Plugin} from "obsidian"
+
+class ggg extends Plugin {
+    // 插件加载时调用
+    async onload() {
+        // 初始化代码
+    }
+
+    // 插件卸载时调用
+    onunload() {
+        // 清理代码
+    }
+}
